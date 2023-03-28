c9d09dfe89c3902d8b71c9376c1f2781bb6fa378
HYPERPC <HYPERPC@DESKTOP-MU1U2GL>
2023-03-28 17:14:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    //задание 1
+    cout << "Home task #2.1\n\n";
+    cout << "Hold fast to dreams\n";
+    cout << "For if dreams die\n";
+    cout << "Life is a broken - winged bird\n";
+    cout << "That cannot fly.\n";
+    cout << "Hold fast to dreams\n";
+    cout << "For when dreams go\n";
+    cout << "Life is a barren field\n";
+    cout << "Frozen with snow."; 
+
+
+
+    //задание 2 
+
+    cout << "Home task #2.2\n\n";
+    cout << "Every" << "\n";
+    cout << "\tHunter" << "\n";
+    cout << "\t\tWants" << "\n";
+    cout << "\t\t\tTo know" << "\n";
+    cout << "\t\t\t\t Where" << "\n";
+    cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+    cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+    return 0;
+}
+
